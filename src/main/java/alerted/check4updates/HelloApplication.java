a6fdc4e8897a74dc669ab1a8a1200a211d43bdca
Zoda69 <JustBecause2882@gmail.com>
2023-02-24 10:10:21 +0200

    Finishing touches, v1.0

--- src/main/java/alerted/check4updates/HelloApplication.java
+++ src/main/java/alerted/check4updates/HelloApplication.java
@@ -12,7 +12,6 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class HelloApplication extends Application {
@@ -22,29 +21,48 @@ public class HelloApplication extends Application {
 
         Parent root = fxmlLoader.load();
 
-        Button upd = (Button) root.lookup("#upd");
-        Label meinVer = (Label) root.lookup("#meinVer");
-        Label lateVer = (Label) root.lookup("#lateVer");
+        Button upd1 = (Button) root.lookup("#upd1");
+        Label meinVer1 = (Label) root.lookup("#meinVer1");
+        Label lateVer1 = (Label) root.lookup("#lateVer1");
+
+        Button upd2 = (Button) root.lookup("#upd2");
+        Label meinVer2 = (Label) root.lookup("#meinVer2");
+        Label lateVer2 = (Label) root.lookup("#lateVer2");
 
         try {
-            File myObj = new File("src/main/resources/ver.txt");
-            Scanner myReader = new Scanner(myObj);
-            String cocks=myReader.nextLine();
-            meinVer.setText(cocks.substring(6,cocks.indexOf("l")));
-            lateVer.setText(cocks.substring(cocks.indexOf(":")+1));
-            myReader.close();
+            File myObj1 = new File("src/main/resources/ver1.txt");
+            Scanner myReader1 = new Scanner(myObj1);
+            File myObj2 = new File("src/main/resources/ver2.txt");
+            Scanner myReader2 = new Scanner(myObj2);
+
+            String cocks1=myReader1.nextLine();
+            meinVer1.setText(cocks1.substring(6,cocks1.indexOf("l")));
+            lateVer1.setText(cocks1.substring(cocks1.indexOf(":")+1));
+
+            String cocks2=myReader2.nextLine();
+            meinVer2.setText(cocks2.substring(6,cocks2.indexOf("l")));
+            lateVer2.setText(cocks2.substring(cocks2.indexOf(":")+1));
+
+            myReader1.close();
+            myReader2.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
-        if(!(lateVer.getText().trim().equals(meinVer.getText().trim())))
+        if(!(lateVer1.getText().trim().equals(meinVer1.getText().trim())))
         {
-            upd.setVisible(true);
-            lateVer.setTextFill(Color.RED);
+            upd1.setVisible(true);
+            lateVer1.setTextFill(Color.RED);
         }
 
-        Scene scene = new Scene(root, 600, 400);
+        if(!(lateVer2.getText().trim().equals(meinVer2.getText().trim())))
+        {
+            upd2.setVisible(true);
+            lateVer2.setTextFill(Color.RED);
+        }
+
+        Scene scene = new Scene(root, 600, 250);
         stage.setTitle("Check4Updates");
         stage.setScene(scene);
         stage.show();
--- src/main/java/alerted/check4updates/checksController.java
+++ src/main/java/alerted/check4updates/checksController.java
@@ -16,31 +16,32 @@ import org.jsoup.select.Elements;
 public class checksController
 {
     @FXML
-    private Label meinVer;
+    private Label meinVer1, meinVer2;
 
     @FXML
-    private Label lateVer;
+    private Label lateVer1, lateVer2;
 
     @FXML
-    private Button upd;
+    private Button upd1, upd2;
     public void ChekingUpdates1()
     {
-        try {
+        try
+        {
             Document doc = Jsoup.connect("https://vsetop.org/games/150-skachat-payday-2-torrent.html").get();
             Elements element = doc.select("div.body > b");
             String text = "";
             for (Element elemented : element)
             {
                 text=elemented.text();
-                if(text.startsWith("Скачать") && !(text.substring(18,28).trim().startsWith(meinVer.getText().trim())))
+                if(text.startsWith("Скачать") && !(text.substring(18,28).trim().startsWith(meinVer1.getText().trim())))
                 {
-                    upd.setVisible(true);
-                    lateVer.setText(text.substring(18,28).trim());
-                    lateVer.setTextFill(Color.RED);
+                    upd1.setVisible(true);
+                    lateVer1.setText(text.substring(18,28).trim());
+                    lateVer1.setTextFill(Color.RED);
                     try {
-                        FileWriter writed = new FileWriter("src/main/resources/ver.txt");
+                        FileWriter writed = new FileWriter("src/main/resources/ver1.txt");
 
-                        writed.write("myver-"+meinVer.getText().trim()+"letver:"+text.substring(18,28).trim());
+                        writed.write("myver-"+meinVer1.getText().trim()+"letver:"+text.substring(18,28).trim());
 
                         writed.close();
                     } catch (FileNotFoundException e) {
@@ -57,9 +58,9 @@ public class checksController
     public void updateE1()
     {
         try {
-            FileWriter writed = new FileWriter("src/main/resources/ver.txt");
+            FileWriter writed = new FileWriter("src/main/resources/ver1.txt");
 
-            writed.write("myver-"+lateVer.getText().trim()+"letver:"+lateVer.getText().trim());
+            writed.write("myver-"+lateVer1.getText().trim()+"letver:"+lateVer1.getText().trim());
 
             writed.close();
         } catch (IOException e) {
@@ -67,8 +68,58 @@ public class checksController
             e.printStackTrace();
         }
 
-        meinVer.setText(lateVer.getText().trim());
-        lateVer.setTextFill(Color.WHITE);
-        upd.setVisible(false);
+        meinVer1.setText(lateVer1.getText().trim());
+        lateVer1.setTextFill(Color.WHITE);
+        upd1.setVisible(false);
+    }
+
+    public void ChekingUpdates2()
+    {
+        try
+        {
+            Document doc = Jsoup.connect("https://vsetop.org/games/1761-my-summer-car.html").get();
+            Elements element = doc.select("div.body > b");
+            String text = "";
+            for (Element elemented : element)
+            {
+                text=elemented.text();
+                if(text.startsWith("Скачать") && !(text.substring(23,33).trim().startsWith(meinVer2.getText().trim())))
+                {
+                    upd2.setVisible(true);
+                    lateVer2.setText(text.substring(23,33).trim());
+                    lateVer2.setTextFill(Color.RED);
+                    try {
+                        FileWriter writed = new FileWriter("src/main/resources/ver2.txt");
+
+                        writed.write("myver-"+meinVer2.getText().trim()+"letver:"+text.substring(18,28).trim());
+
+                        writed.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("An error occurred.");
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateE2()
+    {
+        try {
+            FileWriter writed = new FileWriter("src/main/resources/ver2.txt");
+
+            writed.write("myver-"+lateVer2.getText().trim()+"letver:"+lateVer2.getText().trim());
+
+            writed.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        meinVer2.setText(lateVer2.getText().trim());
+        lateVer2.setTextFill(Color.WHITE);
+        upd2.setVisible(false);
     }
 }
